89926c38fc42fe5535a3e31192f2cc374245bd5b
Ilya Sukharev <imsworkfor@gmail.com>
2023-12-11 23:34:03 +0300

    dep

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const { v4 } = require('uuid');
-const port = process.env.PORT || 5000;
+const port = 5000;
 
 app.listen(port, () =>
   console.log(`Server has been started on port: ${port}...`)
